acc86a21ec0127fcb15ae2e1a85d9ae5687ea86d
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2022-02-23 10:51:25 +0300

    + Students homework

---
+++ modules/info.kgeorgiy.java.advanced.student/info/kgeorgiy/java/advanced/student/Tester.java
@@ -0,0 +1,18 @@
+package info.kgeorgiy.java.advanced.student;
+
+import info.kgeorgiy.java.advanced.base.BaseTester;
+
+/**
+ * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
+ */
+public final class Tester {
+    private Tester() {
+    }
+
+    public static void main(final String... args) {
+        new BaseTester()
+                .add("StudentQuery", StudentQueryTest.class)
+                .add("GroupQuery", GroupQueryTest.class)
+                .run(args);
+    }
+}
